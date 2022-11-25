94e9582fd82d1a837f55389af4213178eb0d1ce1
AlaminDolar <amin15-9898@diu.edu.bd>
2022-11-25 18:37:35 +0600

    footer added

--- src/component/Shared/Footer.js
+++ src/component/Shared/Footer.js
@@ -2,20 +2,18 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <div>
-      <footer className="footer items-center p-4 bg-neutral text-neutral-content">
-        <div className="flex justify-items-center">
-          <div>
-            <p>Shopcart</p>
-          </div>
-          <div className="flex gap-3">
-            <p>About Us</p>
-            <p>Contact</p>
-            <p>help</p>
-          </div>
-          <div>
-            <p>English</p>
-          </div>
+    <div className="pt-10">
+      <footer className="footer pt-10 pb-10 pl-20 pr-20 items-center p-4 bg-neutral text-neutral-content">
+        <div className="items-center grid-flow-col">
+          <p className="text-sm font-medium">shopcart</p>
+        </div>
+        <div className="grid-flow-col gap-4 md:place-self-center md:justify-self-center">
+          <a className="text-sm font-medium">Contact Us</a>
+          <a className="text-sm font-medium">About US</a>
+          <a className="text-sm font-medium">Help</a>
+        </div>
+        <div className="grid-flow-col gap-4 md:place-self-center md:justify-self-end">
+          <p className="text-xl font-medium">English</p>
         </div>
       </footer>
     </div>
